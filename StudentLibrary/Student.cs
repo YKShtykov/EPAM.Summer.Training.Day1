5afc2988abb2fb3f84ee47da7216c35adec66f47
YKShtykov <yakov.shtykov@gmail.com>
2016-07-03 16:36:45 +0300

    initial config

---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentLibrary
+{
+  public class Student
+  {
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Id { get; set; }
+
+    public int Age { get; set; }
+
+    public Student(string firstName, string lastName, int age)
+    {
+      FirstName = firstName;
+      LastName = LastName;
+      Age = Age;
+    }
+
+    public override string ToString()
+    {
+      return string.Format("{0} {1}, {2} years;", FirstName, LastName, Age);
+    }
+  }
+}
---
+++ StudentRepository/Repository.cs
@@ -0,0 +1,18 @@
+﻿using StudentLibrary;
+using System.Collections.Generic;
+namespace StudentRepository
+{
+  public class Repository
+  {
+    private List<Student> students;
+
+    public Repository()
+    {
+      students = new List<Student>();
+    }
+    public void Add(Student student)
+    {
+      students.Add(student);
+    }
+  }
+}
